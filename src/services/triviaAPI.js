e5b27b62a717ed21ccbdbc3d2ebe03e4d0f514e6
Peter Fritz <me@peterfritz.dev>
2022-08-05 19:57:27 -0300

    Requisito 02 - Conclusão do requisito

---
+++ src/services/triviaAPI.js
@@ -0,0 +1,12 @@
+const getToken = async () => {
+  const res = await fetch('https://opentdb.com/api_token.php?command=request');
+  const json = await res.json();
+
+  if (json.response_code) {
+    throw new Error('Não foi possível gerar um token');
+  }
+
+  localStorage.setItem('token', json.token);
+};
+
+export default getToken;
