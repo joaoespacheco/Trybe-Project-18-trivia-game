50b84edb93b81b7c52ac5bdc2e35c627967af160
Peter Fritz <me@peterfritz.dev>
2022-08-09 16:54:51 -0300

    Requisito 09 - Conclusão do requisito

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 import Question from '../components/Question';
+import { saveScore } from '../redux/actions';
 import { getQuestions } from '../services/triviaAPI';
 
 let timer = null;
 
-export default class Game extends Component {
+const MAX_TIME = 30;
+const POINTS = 10;
+const ONE_SECOND = 1000;
+
+class Game extends Component {
   state = {
-    // assertions: 0,
-    score: 0,
     questions: [],
     currentQuestion: 0,
     answered: false,
@@ -33,20 +37,39 @@ export default class Game extends Component {
   }
 
   handleAnswer = (correct) => {
-    console.log(correct);
-
     this.stopTimer();
+
+    this.updateScore(correct);
 
     this.setState({
       answered: true,
     });
   }
 
+  updateScore = (correct) => {
+    const { score, assertions, sendScore } = this.props;
+    const { questions, currentQuestion, elapsedTime } = this.state;
+
+    const difficulties = {
+      hard: 3,
+      medium: 2,
+      easy: 1,
+    };
+
+    const difficulty = difficulties[questions[currentQuestion].difficulty];
+
+    if (correct) {
+      const presentScore = POINTS + ((MAX_TIME - elapsedTime) * difficulty);
+
+      sendScore(score + presentScore, assertions + 1);
+    }
+  }
+
   initTimer = () => {
     timer = setInterval(() => {
       const { elapsedTime } = this.state;
 
-      if (elapsedTime === Number([!+[] + !+[] + !+[]] + [+[]])) {
+      if (elapsedTime === MAX_TIME) {
         this.handleAnswer(false);
         return;
       }
@@ -54,7 +77,7 @@ export default class Game extends Component {
       this.setState({
         elapsedTime: elapsedTime + 1,
       });
-    }, Number([+!+[]] + [+[]] + [+[]] + [+[]]));
+    }, ONE_SECOND);
   }
 
   stopTimer = () => {
@@ -62,12 +85,16 @@ export default class Game extends Component {
     timer = null;
   }
 
+  componentWillUnmount = () => {
+    this.stopTimer();
+  }
+
   render() {
-    const { score, questions, currentQuestion, answered } = this.state;
+    const { questions, currentQuestion, answered } = this.state;
 
     return (
       <main>
-        <Header score={ score } />
+        <Header />
         <div>Game</div>
         {
           questions.length && <Question
@@ -85,4 +112,18 @@ Game.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func,
   }).isRequired,
+  sendScore: PropTypes.func.isRequired,
+  score: PropTypes.string.isRequired,
+  assertions: PropTypes.string.isRequired,
 };
+
+const mapStateToProps = (state) => ({
+  score: state.player.score,
+  assertions: state.player.assertions,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  sendScore: (score, assertions) => dispatch(saveScore(score, assertions)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Game);
