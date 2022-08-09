d1ac061e06b26039cf767f4073f1bb9dc1a36fbe
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-08-09 16:33:31 -0300

    Requisito 13 - Conclusão do requisito

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
+  playerPerformance = () => {
+    const { assertions } = this.props;
+    const minAssertions = 3;
+    if (assertions < minAssertions) {
+      return 'Could be better...';
+    }
+    return 'Well Done!';
+  }
+
   render() {
     return (
-      <div>Feedback</div>
+      <main>
+        <h2 data-testid="feedback-text">{this.playerPerformance()}</h2>
+      </main>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Feedback);
