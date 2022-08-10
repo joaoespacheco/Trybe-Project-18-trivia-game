d20a4adc40f027876f466e6f18e5155d7c67a7f6
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-08-10 15:53:13 -0300

    Requisito 18 - conclusão do requisito

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Ranking extends Component {
   render() {
@@ -7,6 +8,12 @@ export default class Ranking extends Component {
         <h1 data-testid="ranking-title">
           Ranking
         </h1>
+        <Link
+          to="/"
+          data-testid="btn-go-home"
+        >
+          Página Inicial
+        </Link>
       </div>
     );
   }
